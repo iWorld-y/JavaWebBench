8cc0cb3e1d76793b667457dd1bfb057eeb8d3274
Eugene <caima12138@foxmail.com>
2021-09-27 11:12:36 +0800

    begin

--- src/com/iWorld/Main.java
+++ src/com/iWorld/Main.java
@@ -1,8 +1,12 @@
 package com.iWorld;
 
+/***
+ * @author iWorld
+ * @date 2021-9-27 11:09:54
+ * */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
